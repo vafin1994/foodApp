2fe8f478a56afd08f8ad0f2fc3ddd2ff805af7d8
oleksandrvafin <vafinalex@gmail.com>
2022-08-28 22:14:46 +0200

    App init components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {RecipeListComponent} from './recipe-book/recipe-list/recipe-list.component';
+import {RecipeItemComponent} from "./recipe-book/recipe-item/recipe-item.component";
+import { RecipeDetailsComponent } from './recipe-book/recipe-details/recipe-details.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +25,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
